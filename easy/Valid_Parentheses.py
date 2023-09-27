9d415761b135cb327d3898a0209bfbd538c3f550
Saikaisiham <blackbutlersiham2001@gmail.com>
2023-09-27 07:23:27 +0100

    commit

--- easy/Valid_Parentheses.py
+++ easy/Valid_Parentheses.py
@@ -1,5 +1,21 @@
 class Solution :
+    """
+    Initialize the solution class 
+    Args : 
+    Attributes : 
+
+    """
     def are_pair(self, open , close):
+        """
+        Check if the given open and close characters form a valid pair.
+
+        Args:
+            open (str): The open character.
+            close (str): The close character.
+
+        Returns:
+            bool: True if the characters form a valid pair, False otherwise.
+        """
         if open == '(' and close == ')':
             return True
         if open == '[' and close == ']':
@@ -9,6 +25,15 @@ class Solution :
         
 
     def valid_parentheses(self, expression):
+        """
+        Check the validity of a parentheses expression.
+
+        Args:
+            expression (str): The input expression to be validated.
+
+        Returns:
+            bool: True if the expression is valid, False otherwise.
+        """
         stack =[]
         for exp in expression: 
             if exp == '(' or exp =='{' or exp == '[':
